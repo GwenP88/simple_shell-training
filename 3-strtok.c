c2eab78b5c18def31aaac4d2b98a216e6d094dc0
GwenP88 <12777@holbertonstudents.com>
2025-12-16 12:30:00 +0100

    add comment on code

--- 3-strtok.c
+++ 3-strtok.c
@@ -6,6 +6,12 @@
 
 char **empty_array(void);
 
+/**
+ * split_string - split a string into words and return a NULL-terminated array
+ * @string: input string to split (words are separated by spaces/tabs/newlines)
+ * Return: pointer to a NULL-terminated array of words, or NULL on failure
+ */
+
 char **split_string (char *string)
 {
 	char *str1, *str2, *token;
@@ -13,21 +19,26 @@ char **split_string (char *string)
 	char **tokens;
 	size_t count, i, j;
 
-	separator = " \t\n";
+	/* Délimiteurs: espace, tab, retour ligne */
+	separator = " \t\n"; 
 
+	/* Erreur: entrée NULL */
 	if (string == NULL)
 		return (NULL);
 	
+	/* Passage 1 (comptage): On duplique la chaîne car strtok() modifie le buffer */
 	str1= strdup (string);
 	if (str1 == NULL)
 		return (NULL);
 	
+	/* Cas chaîne vide => tableau vide (pas une erreur) */
 	if (*str1 == '\0')
 	{
 		free(str1);
 		return empty_array();
 	}
 
+	/* Comptage des mots: token pointe dans str1 (buffer modifié). On incrémente count par token */
 	count = 0;
 	token = strtok (str1, separator);
 	while (token != NULL)
@@ -36,6 +47,7 @@ char **split_string (char *string)
 		token = strtok(NULL, separator);
 	}
 
+	/* Cas: uniquement des séparateurs => 0 token - On retourne un tableau vide (pas NULL) */
 	if (count == 0)
 	{
 		free(str1);
@@ -43,10 +55,12 @@ char **split_string (char *string)
 	}
 	free (str1);
 
+	/* Passage 2 (remplissage): Nouvelle copie car strtok() a consommé modifié la première */
 	str2= strdup (string);
 	if (str2 == NULL)
 		return (NULL);
 
+	/* Allocation du tableau final: count mots + 1 case pour le NULL terminal */	
 	tokens = malloc ((count + 1) * sizeof(char *));
 	if (tokens == NULL)
 	{
@@ -54,6 +68,7 @@ char **split_string (char *string)
 		return (NULL);
 	}
 
+	/* Remplissage: Chaque token est dupliqué avec strdup() */
 	i = 0;
 	token = strtok(str2, separator);
 	while (token != NULL)
@@ -70,10 +85,17 @@ char **split_string (char *string)
 		i++;
 		token = strtok(NULL, separator);
 	}
+	/* ajout du marqueur de fin du tableau */
 	tokens[i] = NULL;
 	free (str2);
 	return (tokens);
 }
+
+
+/**
+ * empty_array - allocate and return an empty NULL-terminated array of strings
+ * Return: pointer to a NULL-terminated array on success, or NULL on failure
+ */
 
 char **empty_array(void)
 {
