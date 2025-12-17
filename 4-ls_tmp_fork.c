059b8685a0536666d80a4619f142b9a2cbcbbb00
GwenP88 <12777@holbertonstudents.com>
2025-12-17 16:26:44 +0100

    add comment on programme ls_tmp_fork

--- 4-ls_tmp_fork.c
+++ 4-ls_tmp_fork.c
@@ -6,8 +6,16 @@
 
 extern char **environ;
 
+/**
+ * main - creates 5 child processes to execute "ls -l /tmp"
+ * Return: 0 on success, exits with a non-zero value on error
+ */
+
+/* environnement du process courant */
+
 int main(void)
 {
+	/* chemin absolu du binaire à exécuter */
 	const char *path = "/usr/bin/ls";
 	char *argv_exec [4] = {"ls", "-l", "/tmp", NULL};
 	int i, status;
@@ -16,33 +24,37 @@ int main(void)
 	/* boucler 5 fois*/
 	for (i = 0; i < 5; i++)
 	{
-		/* fork*/
+		/* duplication du process courant */
 		pid = fork();
-		/* test fork : -1 = echec du fork */
+
+		/* fork échoue : erreur système, on stoppe tout */
 		if (pid == -1)
 		{
 			perror("fork");
 			exit(EXIT_FAILURE);
 		}
-		 /* fork ok et dans processus fils*/
+		/* fork ok et dans processus fils*/
 		if (pid == 0)
 		{
+			/* remplacement du code du fils par ls -l /tmp */
 			execve(path, argv_exec, environ);
 			/* si on arrive ici c'est forcément qu'il y a erreur*/
 			perror(path);
-    		exit(127);
+			exit(127);
 		}
 		/* fork ok et dans processus père*/
 		if (pid > 0)
 		{
+			/* attente du fils courant avant d’en créer un autre */
 			if (waitpid(pid, &status, 0) == -1)
 			{
 				perror("waitpid");
-            	exit(EXIT_FAILURE);	
+				exit(EXIT_FAILURE);
 			}
+			/* si le fils ne s’est pas terminé normalement */
 			if (!WIFEXITED(status))
 				exit(EXIT_FAILURE);
-			
+			/* si le fils a quitté avec un code d’erreur */
 			if (WEXITSTATUS(status) != 0)
 				exit(EXIT_FAILURE);
 		}
