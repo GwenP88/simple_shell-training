a5cd2d7f0e072edc1660e6b8c0e2e6aff7a8e384
GwenP88 <12777@holbertonstudents.com>
2025-12-20 19:36:29 +0100

    add comment and descriptions functions

--- simple_shell_v_00.2/shell.c
+++ simple_shell_v_00.2/shell.c
@@ -1,25 +1,30 @@
 #include "shell.h"
 
 /**
- * main - simple shell loop that executes commands given by path (no arguments)
+ * main - runs a simple interactive loop
+ * that reads a command line and executes
+ * it as a path-only command (no arguments)
  *
  * Return: 0 on success.
  */
 
 int main(void)
 {
+	size_t len = 0;
+	ssize_t read;
+	char *lineptr = NULL;
 
 	while (1)
 	{
-		display_prompt();
+		write(1, "#cisfun$ ", 9);
 		read = read_command(&lineptr, &len);
-		if(read == -1 )
+		if (read == -1)
 		{
 			write(1, "\n", 1);
 			break;
 		}
 		delete_newline(lineptr, read);
-		if(is_empty_line(lineptr) == 1)
+		if (is_empty_line(lineptr))
 			continue;
 		execute_command(lineptr);
 	}
@@ -27,10 +32,16 @@ int main(void)
 	return (0);
 }
 
-void display_prompt(void)
-{
-	write(1, "#cisfun$ ", 9);
-}
+/**
+ * read_command - reads a command line
+ * from standard input using getline
+ * @lineptr: address of the buffer pointer
+ * where the line will be stored/updated
+ * @len: address of the buffer size used by getline
+ *
+ * Return: number of characters read, or -1 on EOF/error.
+ */
+
 
 ssize_t read_command(char **lineptr, size_t *len)
 {
@@ -40,23 +51,48 @@ ssize_t read_command(char **lineptr, size_t *len)
 	return (read);
 }
 
+/**
+ * delete_newline - removes the trailing newline character
+ * from an input line
+ * @lineptr: input string to sanitize
+ * @read: number of characters read (returned by getline)
+ *
+ * Return: nothing.
+ */
+
 void delete_newline(char *lineptr, ssize_t read)
 {
-	if(lineptr == NULL)
+	if (lineptr == NULL)
 		return;
-	if(read > 0 && lineptr[read - 1] == '\n')
+	if (read > 0 && lineptr[read - 1] == '\n')
 		lineptr[read - 1] = '\0';
 }
 
+/**
+ * is_empty_line - checks whether a given line is empty
+ * @lineptr: string to check
+ *
+ * Return: 1 if the line is NULL or empty, otherwise 0.
+ */
+
 int is_empty_line(const char *lineptr)
 {
-	if(lineptr == NULL)
+	if (lineptr == NULL)
 		return (1);
-	else if(lineptr[0] == '\0')
+	else if (lineptr[0] == '\0')
 		return (1);
 	else
 		return (0);
 }
+
+/**
+ * execute_command - creates a child process
+ * and executes a command using execve
+ * @cmd: command path to execute (no arguments)
+ *
+ * Return: 0 if the command exits successfully, otherwise 1.
+ */
+
 
 int execute_command(char *cmd)
 {
